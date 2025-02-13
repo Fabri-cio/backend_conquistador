226260d391b053b19b066a96acac8252a128a58c
wil <wil.fabri777@gmail.com>
2025-02-13 01:19:58 -0400

    prueba con DATABASE_PUBLIC_URL

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -130,7 +130,7 @@ REST_FRAMEWORK = {
 print(os.environ.get('DATABASE_URL'))
 DATABASES = {    
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+        default=os.environ.get('DATABASE_PUBLIC_URL')
     )
 }
 
