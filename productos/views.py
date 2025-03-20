4df6600bafab207a6c6e9c995774e197795858b8
wil <wil.fabri777@gmail.com>
2025-03-19 23:12:50 -0400

    paginacion pero hay que arreglar la vista de productos

--- productos/views.py
+++ productos/views.py
@@ -1,6 +1,15 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework import viewsets
 from .serializer import CategoriaSerializer, ProveedorSerializer, ProductoSerializer
 from .models import Categoria, Proveedor, Producto
+from rest_framework.pagination import PageNumberPagination
+
+# Definir una clase de paginación personalizada si quieres ajustar el tamaño de la página y el comportamiento
+class PaginacionPersonalizada(PageNumberPagination):
+    page_size = 10  # Número predeterminado de elementos por página
+    page_size_query_param = 'page_size'  # Permitir cambiar el tamaño de la página desde los parámetros de la consulta
+    max_page_size = 100  # Tamaño máximo de página permitido
 
 class CategoriaView(viewsets.ModelViewSet):
     serializer_class = CategoriaSerializer
@@ -12,4 +21,16 @@ class ProveedorView(viewsets.ModelViewSet):
 
 class ProductoView(viewsets.ModelViewSet):
     serializer_class = ProductoSerializer
-    queryset = Producto.objects.select_related('categoria', 'id_proveedor')
+    queryset = Producto.objects.all().order_by('id_producto')  # Ordena por id_producto en lugar de id
+
+    pagination_class = PaginacionPersonalizada
+
+    def list(self, request, *args, **kwargs):
+        all_data = request.query_params.get('all_data', 'false').lower() == 'true'  # Convierte a booleano correctamente
+
+        if all_data:
+            queryset = self.get_queryset()
+            serializer = self.get_serializer(queryset, many=True)
+            return Response({'data': serializer.data}, status=status.HTTP_200_OK)
+
+        return super().list(request, *args, **kwargs)  # Usa la paginación normal
