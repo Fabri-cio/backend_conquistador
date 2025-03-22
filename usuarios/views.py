1a4f121e5f353e0f89292a29b04f0c593f1aca40
wil <wil.fabri777@gmail.com>
2025-03-21 23:31:51 -0400

    se agrego paginacion para usuarios y se agrego datos a la database

--- usuarios/views.py
+++ usuarios/views.py
@@ -7,8 +7,16 @@ from rest_framework.response import Response
 from django.contrib.auth import get_user_model, authenticate
 from knox.models import AuthToken
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
+from rest_framework import status
 
 User = get_user_model()
+
+# Definir una clase de paginación personalizada si quieres ajustar el tamaño de la página y el comportamiento
+class PaginacionPersonalizada(PageNumberPagination):
+    page_size = 10  # Número predeterminado de elementos por página
+    page_size_query_param = 'page_size'  # Permitir cambiar el tamaño de la página desde los parámetros de la consulta
+    max_page_size = 100  # Tamaño máximo de página permitido
 
 class LoginViewset(viewsets.ViewSet):
     permission_classes = [permissions.AllowAny]
@@ -63,6 +71,18 @@ class UserViewset(viewsets.ViewSet):
         return Response(serializer.data)
     
 class CustomUserViewSet(viewsets.ModelViewSet):
-    queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
     permission_classes = [permissions.AllowAny]
+    queryset = CustomUser.objects.all().order_by('id')  # Ordena por id_producto en lugar de id
+
+    pagination_class = PaginacionPersonalizada
+
+    def list(self, request, *args, **kwargs):
+        all_data = request.query_params.get('all_data', 'false').lower() == 'true'  # Convierte a booleano correctamente
+
+        if all_data:
+            queryset = self.get_queryset()
+            serializer = self.get_serializer(queryset, many=True)
+            return Response({'data': serializer.data}, status=status.HTTP_200_OK)
+
+        return super().list(request, *args, **kwargs)  # Usa la paginación normal
