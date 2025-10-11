022078585b1a40106c1f1ad8443c8df421788ec6
wil <wil.fabri777@gmail.com>
2025-10-10 21:27:42 -0400

    cambio de url

--- usuarios/models.py
+++ usuarios/models.py
@@ -70,7 +70,7 @@ def password_reset_token_created(reset_password_token, *args, **kwargs):
     msg = EmailMultiAlternatives(
         subject=f"Request for resetting password for {reset_password_token.user.email}", 
         body=plain_message,
-        from_email="sender@example.com", 
+        from_email=os.getenv("EMAIL_HOST_USER"), 
         to=[reset_password_token.user.email]
     )
 
