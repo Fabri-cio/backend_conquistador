e48660d950ad61cae5556d7d13a2ab51d14fa4cb
wil <wil.fabri777@gmail.com>
2025-10-15 21:36:54 -0400

    prueba para importar datos

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 # core/models.py
 from django.db import models
+from django.utils import timezone
 
 class AuditoriaBase(models.Model):
     usuario_creacion = models.ForeignKey(
@@ -14,7 +15,8 @@ class AuditoriaBase(models.Model):
         null=True,
         related_name="%(class)s_modificados"
     )
-    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_creacion = models.DateTimeField(default=timezone.now)
+    # fecha_creacion = models.DateTimeField(auto_now_add=True)
     fecha_modificacion = models.DateTimeField(blank=True, null=True)
     comentario_modificacion = models.TextField(blank=True, null=True)
 
